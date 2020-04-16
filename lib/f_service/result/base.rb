9b58a7edcf1bb25a9ed6bee12229bc3c0caa19f2
Matheus Richard <matheusrichardt@gmail.com>
2020-04-16 18:37:03 -0300

    Add docs for Result::Base

--- lib/f_service/result/base.rb
+++ lib/f_service/result/base.rb
@@ -2,6 +2,9 @@
 
 module FService
   module Result
+    # Abstract base class for Result::Success and Result::Failure.
+    #
+    # @abstract
     class Base
       %i[initialize then successful? failed? value value! error].each do |method_name|
         define_method(method_name) do |*_args|
@@ -9,6 +12,30 @@ module FService
         end
       end
 
+      # "Pattern matching"-like method for results.
+      # It will run the success path if Result is a Success.
+      # Otherwise, it will run the failure path.
+      #
+      #
+      # @example
+      #   class UsersController < BaseController
+      #     def update
+      #       User::Update.(user: user).on(
+      #         success: ->(value) { return json_success(value) },
+      #         failure: ->(error) { return json_error(error) }
+      #       )
+      #     end
+      #
+      #     private
+      #
+      #     def user
+      #       @user ||= User.find_by!(slug: params[:slug])
+      #     end
+      #   end
+      #
+      # @param success [#call] a lambda (or anything that responds to #call) to run on success
+      # @param failure [#call] a lambda (or anything that responds to #call) to run on failure
+      # @api public
       def on(success:, failure:)
         if successful?
           success.call(value)
