06216d27b35c619c196d2e7b69443b259ea7337e
Matheus Richard <matheusrichardt@gmail.com>
2020-04-17 18:02:56 -0300

    Improve docs for failure and success

--- lib/f_service/result/failure.rb
+++ lib/f_service/result/failure.rb
@@ -5,26 +5,39 @@ require_relative '../errors'
 
 module FService
   module Result
+    # Represents a value of a failed operation.
+    #
+    # @api public
     class Failure < Result::Base
+      # Returns the provided error
       attr_reader :error
 
+      # Creates a failed operation.
+      # You usually shouldn't call this directly. See {FService::Base#failure}.
+      #
+      # @param error [Object] failure value.
       def initialize(error)
         @error = error
         freeze
       end
 
+      # Returns false.
       def successful?
         false
       end
 
+      # Returns true.
       def failed?
         true
       end
 
+      # Failed operations do not have value.
       def value
         nil
       end
 
+      # Raises an exception if called.
+      # (see #value)
       def value!
         raise Result::Error, 'Failure objects do not have value'
       end
--- lib/f_service/result/success.rb
+++ lib/f_service/result/success.rb
@@ -4,30 +4,45 @@ require_relative 'base'
 
 module FService
   module Result
+    # Represents a value of a successful operation.
+    #
+    # @api public
     class Success < Result::Base
+      # Returns the provided value.
       attr_reader :value
 
+      # Creates a successful operation.
+      # You usually shouldn't call this directly. See {FService::Base#success}.
+      #
+      # @param value [Object] success value.
       def initialize(value)
         @value = value
         freeze
       end
 
+      # Returns true.
       def successful?
         true
       end
 
+      # Returns false.
       def failed?
         false
       end
 
+      # (see #value)
       def value!
         value
       end
 
+      # Succesful operations do not have error.
+      #
+      # @return [nil]
       def error
         nil
       end
 
+      # @param [Block]
       def then
         yield value
       end
