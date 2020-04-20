2e6b5407452bb56fc3cf2b5eed5c9774349bfa64
Matheus Richard <matheusrichardt@gmail.com>
2020-04-20 13:15:53 -0300

    Add docs for error classes

--- lib/f_service/errors.rb
+++ lib/f_service/errors.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 module FService
+  # General FService error
   class Error < StandardError; end
 
   module Result
+    # Fservice::Result related error
     class Error < Error; end
   end
 end
