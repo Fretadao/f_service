e00648652f1479ee74264d4c8d926f948427ae88
Matheus Richard <matheusrichardt@gmail.com>
2020-08-11 18:29:19 -0300

    Add `deprecate!` method

--- lib/f_service.rb
+++ lib/f_service.rb
@@ -6,4 +6,10 @@ require_relative 'f_service/base'
 #
 # @api public
 module FService
+  # Mark a method as deprecated
+  def self.deprecate!(name:, alternative:)
+    warn "[DEPRECATED] #{name} is deprecated; " \
+         "use #{alternative} instead. " \
+         'It will be removed on the next release.'
+  end
 end
