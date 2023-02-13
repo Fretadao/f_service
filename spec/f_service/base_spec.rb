bbfcb7beb7a7ea3c7ebba615b6cc79d8ccb8c4e1
Bruno Vicenzo <bruno.vicenzo@fretadao.com.br>
2023-02-13 11:54:21 -0300

    Fix: Remove rails method matcher

--- spec/f_service/base_spec.rb
+++ spec/f_service/base_spec.rb
@@ -82,7 +82,7 @@ RSpec.describe FService::Base do
       subject(:response) { described_class.new.Try { 0 / 1 } }
 
       it { expect(response).to be_successful }
-      it { expect(response.types).to be_blank }
+      it { expect(response.types).to be_empty }
       it { expect(response.value!).to be_zero }
     end
 
